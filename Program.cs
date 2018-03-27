6ffdd45311453bfd072be59c2f7ed930beb5dc22
Daniel Rużewicz <d.ruzew@gmail.com>
2018-03-27 13:01:17 +0200

    Added unsortedArray, Added empty method bubbleSort, Added method to print Array in Console

--- Program.cs
+++ Program.cs
@@ -10,6 +10,45 @@ namespace SortingExercises
     {
         static void Main(string[] args)
         {
+            List<int> unsortedArray = new List<int>()
+            {
+                9,
+                13,
+                7,
+                14,
+                5,
+                19,
+                6,
+                2,
+                11,
+                1,
+                8,
+                18,
+                12
+            };
+            PrintArray(unsortedArray);
+
+            BubbleSort(unsortedArray);
+
+            PrintArray(unsortedArray);
+
+            Console.ReadKey();
+
+        }
+
+        private static void PrintArray(List<int> unsortedArray)
+        {
+            foreach (var integer in unsortedArray)
+            {
+                Console.Write(integer + ", ");
+            }
+            Console.WriteLine("----------------");
+            Console.WriteLine("");
+        }
+
+        private static void BubbleSort(List<int> unsortedArray)
+        {
+            
         }
     }
 }
